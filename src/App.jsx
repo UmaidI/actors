9e146ec1c3ae01a46691c2c8f434bb3a7d54f0cf
Umaid Ismail <umaidismail@umaids-air.mynetworksettings.com>
2025-07-08 00:37:25 -0700

    creat POST request to API

--- src/App.jsx
+++ src/App.jsx
@@ -3,36 +3,100 @@ import { useEffect, useState } from "react"
 const App = () => {
   const [actors, setActors] = useState([]);
   
-  const [emailInput, setEmailInput] = useState('')
+  const [emailInput, setEmailInput] = useState('');
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [role, setRole] = useState("");
+  const [avatar, setAvatar] = useState("");
+  
   useEffect(() => {
     const getActors = async () => {
-      const response = await fetch("https://api.escuelajs.co/api/v1/users");
-      const users = await response.json();
-      setActors(users);
+      try{
+        const response = await fetch("https://api.escuelajs.co/api/v1/users");
+        const users = await response.json();
+        setActors(users);
+      } catch(err){
+        console.log(err);
+      }
     }
     getActors();
   }, []);
 
+  const createUser = async (event) => {
+    event.preventDefault();
+    // console.log(emailInput);
+    // console.log(name);
+    // console.log(password);
+    // console.log(role);
+    // console.log(avatar);
+  
+    try{
+      const response = await fetch("https://api.escuelajs.co/api/v1/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          email: emailInput,
+          name: name,
+          password: password,
+          role: role,
+          avatar: avatar
+        })
+      })
+
+      const newActor = await response.json();
+      const newActorsArray = [...actors, newActor];
+      setActors(newActorsArray);
+      
+    } catch(err){
+      console.log(err);
+    }
+  
+  }
+
+
   return (
     <>
       <h1>Create a new User</h1>
-      <form>
+      <form onSubmit={createUser}>
       
       <input 
-      type = "email" 
-      placeholder="email" 
-      onChange={(event)=> {setEmailInput(event.target.value)}}
+        type = "email" 
+        placeholder="email" 
+        onChange={(event)=> {setEmailInput(event.target.value)}}
       
       ></input>
       
 
-      <input placeholder="name"></input>
+      <input 
+        placeholder="name"
+        onChange={(event) => setName(event.target.value)}  
+      >
+        
+      </input>
 
-      <input type="password" placeholder="password"></input>
+      <input 
+        type="password" 
+        placeholder="password"
+        onChange={(event) => setPassword(event.target.value)}
+      >
 
-      <input placeholder="role"></input>
+      </input>
 
-      <input placeholder="avatar"></input>
+      <input 
+        placeholder="role"
+        onChange={(event) => setRole(event.target.value)}  
+      >
+        
+      </input>
+
+      <input 
+        placeholder="avatar"
+        onChange={(event) => setAvatar(event.target.value)}  
+      >
+        
+      </input>
 
       <button>Create User</button>
       </form>
